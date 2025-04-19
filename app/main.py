765b09eb5fe10bce718f2c421d3c1bb2e16a72f0
Priyanshu Kumar <priyanshuknp444@gmail.com>
2025-04-19 11:59:21 +0530

    add upload file and chat route

--- app/main.py
+++ app/main.py
@@ -1,10 +1,14 @@
 from dotenv import load_dotenv
 load_dotenv()
 
-
-from fastapi import FastAPI
+from pathlib import Path
+import shutil
+from fastapi import Body, FastAPI, File, UploadFile
 from fastapi.middleware.cors import CORSMiddleware
-
+from helper import chat_llm, get_relevent_chunks, get_system_prompt, load_file, split_file, store_embeddings_vector_store
+from embeddings_model import embeddings
+from retriever import get_retriever
+import uuid
 
 app = FastAPI()
 
@@ -17,8 +21,59 @@ app.add_middleware(
 )
 
 
-@app.get("/")
-def root():
-    return {
-        "message": "Hello World"
-    }
+@app.post("/api/upload")
+def upload_file(user_id: str = Body(...), file: UploadFile = File(...)):
+    try:
+        upload_dir = Path("files")
+        upload_dir.mkdir(exist_ok=True)
+       
+        user_id = user_id or str(uuid.uuid4())
+       
+        # save this file
+        file_path = upload_dir / f"{user_id}::{file.filename}"
+       
+        with open(file_path, "wb") as buffer:
+            shutil.copyfileobj(file.file, buffer)
+        
+        docs = load_file(f"{user_id}::{file.filename}")
+        split_docs = split_file(docs)
+       
+        #store embedding on store
+        store_embeddings_vector_store(embeddings, split_docs, user_id)
+
+        print("Embedding stored successfully")
+
+        return {
+            "status": "success", 
+            "filename": file.filename,
+            "user_id" : user_id
+        }
+    except Exception as e:
+        print(e)
+        return {"status": "error", "detail": str(e)}
+
+
+@app.post("/api/chat")
+async def chat(payload: dict):
+    try:
+        user_id = payload.get("user_id")
+        query = payload.get("query")
+        
+        retriever = get_retriever(embeddings, user_id)
+
+        relevant_chunks = get_relevent_chunks(retriever, query) 
+
+        system_prompt = get_system_prompt(relevant_chunks)
+
+        # Chat
+        response = chat_llm(system_prompt, query)
+
+        return {
+            "status" : "success",
+            "content" : response.content
+        }
+    except Exception as e:
+        return {
+            "status": "error", 
+            "detail": str(e)
+        }
